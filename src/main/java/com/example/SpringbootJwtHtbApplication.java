9c30b106d145fc16b01ef3248110ee21d9440579
dat nguyen <thedatnguyen20@gmail.com>
2021-09-22 10:58:22 +0700

    entity

---
+++ src/main/java/com/example/SpringbootJwtHtbApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtHtbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtHtbApplication.class, args);
+	}
+
+}
