9dff0625dc55c50f625e93c5606f285e25eb5e11
dat nguyen <thedatnguyen20@gmail.com>
2021-09-22 10:59:18 +0700

    register

---
+++ src/main/java/com/example/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.example.controller;
+
+import com.example.authen.UserPrincipal;
+import com.example.entity.Token;
+import com.example.entity.User;
+import com.example.service.TokenService;
+import com.example.service.UserService;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+
+        return userService.createUser(user);
+    }
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+import com.example.authen.UserPrincipal;
+import com.example.entity.User;
+
+public interface UserService {
+    User createUser(User user);
+    UserPrincipal findByUsername(String username);
+}
